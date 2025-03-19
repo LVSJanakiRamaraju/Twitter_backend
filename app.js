8f6377b5b4c755bc95b33c7fd7dbeeb82b153ac4
LVSJanakiRamaraju <rajakanumuri2005@example.com>
2025-03-19 15:13:53 +0000

    Added Login user

--- app.js
+++ app.js
@@ -75,6 +75,57 @@ const authenticateToken = (request, response, next) => {
   }
 }
 
+//API: Login User
+app.post('/login/', async (request, response) => {
+  const {username, password} = request.body
+  const payLoad = {username}
+  const jwtToken = jwt.sign(payLoad, 'SECRET_KEY')
+  const userCheckQuery = `
+    SELECT * FROM user WHERE username = '${username}';`
+  const dbUser = await db.get(userCheckQuery)
+  if (dbUser === undefined) {
+    response.status(400)
+    response.send('Invalid user')
+  } else {
+    const isPasswordMatches = await bcrypt.compare(password, dbUser.password)
+    if (isPasswordMatches) {
+      response.send({jwtToken})
+    } else {
+      response.status(400)
+      response.send('Invalid password')
+    }
+  }
+})
+const isUserFollowing = async (request, response, next) => {
+  const {tweetId} = request.params
+  const {username} = request.headers
+  const getUserQuery = `
+    SELECT * FROM user WHERE username = '${username}';`
+  const dbUser = await db.get(getUserQuery)
+  const userId = dbUser['user_id']
+  const followingQuery = `SELECT following_user_id FROM follower WHERE follower_user_id = ${userId};`
+  const userFollowingData = await db.all(followingQuery)
+  // console.log(userFollowingData);
+
+  const tweetUserIdQuery = `
+    SELECT * FROM tweet WHERE tweet_id = ${tweetId}`
+  const tweetData = await db.get(tweetUserIdQuery)
+  const tweetUserID = tweetData['user_id']
+
+  let isTweetUSerIDInFollowingIds = false
+  userFollowingData.forEach(each => {
+    if (each['following_user_id'] === tweetUserID) {
+      isTweetUSerIDInFollowingIds = true
+    }
+  })
+
+  if (isTweetUSerIDInFollowingIds) {
+    next()
+  } else {
+    response.status(401)
+    response.send('Invalid Request')
+  }
+}
 
 
 
